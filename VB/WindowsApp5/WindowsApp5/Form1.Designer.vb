2020c847193303a2131b8cc9f440ea8279214b08
andrew <ajder706@learner.dvusd.org>
2022-10-12 12:39:08 -0700

    10/12

---
+++ VB/WindowsApp5/WindowsApp5/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource2 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource3 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource4 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource5 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource6 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource7 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource8 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource9 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource10 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource11 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource12 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource13 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource14 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource15 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource16 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource17 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource18 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource19 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource20 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource21 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource22 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource23 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource24 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource25 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource26 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource27 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource28 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource29 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource30 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource31 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource32 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource33 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource34 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource35 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource36 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource16, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource17, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource18, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource19, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource20, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource21, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource22, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource23, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource24, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource25, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource26, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource27, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource28, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource29, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource30, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource31, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource32, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource33, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource34, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource35, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource36, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApp5.My.Resources.Resources.download__1_
+        Me.PictureBox1.Location = New System.Drawing.Point(228, 24)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(165, 117)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.UseWaitCursor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(425, 161)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.UseWaitCursor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(228, 147)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(165, 69)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "CLICK"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseWaitCursor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.Label1.Location = New System.Drawing.Point(422, 142)
+        Me.Label1.Name = "Label1"
+        Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label1.Size = New System.Drawing.Size(14, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "u"
+        Me.Label1.UseWaitCursor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(517, 303)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(157, 58)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "GAME"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.UseWaitCursor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Text = "u"
+        Me.UseWaitCursor = True
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource16, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource17, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource18, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource19, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource20, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource21, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource22, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource23, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource24, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource25, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource26, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource27, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource28, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource29, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource30, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource31, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource32, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource33, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource34, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource35, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource36, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents BindingSource2 As BindingSource
+    Friend WithEvents BindingSource3 As BindingSource
+    Friend WithEvents BindingSource4 As BindingSource
+    Friend WithEvents BindingSource5 As BindingSource
+    Friend WithEvents BindingSource6 As BindingSource
+    Friend WithEvents BindingSource7 As BindingSource
+    Friend WithEvents BindingSource8 As BindingSource
+    Friend WithEvents BindingSource9 As BindingSource
+    Friend WithEvents BindingSource10 As BindingSource
+    Friend WithEvents BindingSource11 As BindingSource
+    Friend WithEvents BindingSource12 As BindingSource
+    Friend WithEvents BindingSource13 As BindingSource
+    Friend WithEvents BindingSource14 As BindingSource
+    Friend WithEvents BindingSource15 As BindingSource
+    Friend WithEvents BindingSource16 As BindingSource
+    Friend WithEvents BindingSource17 As BindingSource
+    Friend WithEvents BindingSource18 As BindingSource
+    Friend WithEvents BindingSource19 As BindingSource
+    Friend WithEvents BindingSource20 As BindingSource
+    Friend WithEvents BindingSource21 As BindingSource
+    Friend WithEvents BindingSource22 As BindingSource
+    Friend WithEvents BindingSource23 As BindingSource
+    Friend WithEvents BindingSource24 As BindingSource
+    Friend WithEvents BindingSource25 As BindingSource
+    Friend WithEvents BindingSource26 As BindingSource
+    Friend WithEvents BindingSource27 As BindingSource
+    Friend WithEvents BindingSource28 As BindingSource
+    Friend WithEvents BindingSource29 As BindingSource
+    Friend WithEvents BindingSource30 As BindingSource
+    Friend WithEvents BindingSource31 As BindingSource
+    Friend WithEvents BindingSource32 As BindingSource
+    Friend WithEvents BindingSource33 As BindingSource
+    Friend WithEvents BindingSource34 As BindingSource
+    Friend WithEvents BindingSource35 As BindingSource
+    Friend WithEvents BindingSource36 As BindingSource
+    Friend WithEvents Button2 As Button
+End Class
---
+++ VB/WindowsApp5/WindowsApp5/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Dim car As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        car = car + 1
+        TextBox1.Text = car
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form2.Show()
+
+    End Sub
